28defa1d477d1c72e2ef114d25ad03a30e5c70bf
SwethaReddySalkuti <salkutiswethareddy@gmail.com>
2023-09-18 10:32:35 +0530

    Associations

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const express=require('express');
 const bodyParser=require('body-parser');   // parses string based client request into js Object
 const cors=require('cors');  // allows restricted resources on a web page to be accessed from another domain
 const http =require ("http");
-
+const socketIO=require('socket.io')
 
 const app=express();
+const server = http.createServer(app);
+const io= socketIO(server,{ cors : { origin : '*'}});  // persistent connection between socket & client 
+//instead of many requets ,only one connection
 
+io.on("connection",(socket)=>{
+    console.log('Socket.io Connected');
+    socket.on("message",(msg,userName,groupId,userId)=>{
+        socket.broadcast.emit("message",msg,userName,groupId,userId)       //except the one being called
+    });
+    socket.on("file",(message,userName,groupId,userId)=>{
+        socket.broadcast.emit("file",message,userName,groupId,userId)
+
+    })
+})
 
 
 require('dotenv').config()
@@ -16,6 +29,11 @@ const sequelize=require('./utils/database');
 const userRoutes=require('./routes/user');
 const chatRoutes=require('./routes/chat');
 const groupRoutes=require('./routes/group')
+const User=require('./models/user');
+const Message=require('./models/chat');
+const Group=require('./models/group');
+const Forgotpassword = require('./models/forgotpassword');
+const UserGroup=require('./models/usergroup');
 
 app.use(cors({
     origin:'*'  // any other domain can access the resources
@@ -29,7 +47,19 @@ app.use(groupRoutes);
 app.use('/password', resetPasswordRoutes);
 
 
+// Associations
 
+User.hasMany(Message);
+Message.belongsTo(User);
+
+Group.belongsToMany(User,{through:UserGroup});
+User.belongsToMany(Group,{through:UserGroup});            // tables which contains only foriegn keys
+
+Group.hasMany(Message);
+Message.belongsTo(Group);
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
 
 sequelize.sync(
     //  {force : true}
